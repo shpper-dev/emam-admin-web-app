46375ee3d74a2f37a25ca7f2703f9b9183013167
Hafizur Rahman <hafizur.rahman.cs@gmail.com>
2026-07-03 00:41:45 +0600

    Mobile initial scaffold setup

--- lib/responsive/mobile_scaffold.dart
+++ lib/responsive/mobile_scaffold.dart
@@ -1,4 +1,6 @@
+import 'package:emam_admin_web_app/constants/app_constants.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MobileScaffold extends StatefulWidget {
   const MobileScaffold({super.key});
@@ -10,6 +12,38 @@ class MobileScaffold extends StatefulWidget {
 class _MobileScaffoldState extends State<MobileScaffold> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.red);
+    return Scaffold(
+      backgroundColor: AppConstants.bgColor,
+      appBar: AppBar(
+        title: Text(
+          'Emam Admin Panel',
+          style: GoogleFonts.aBeeZee(color: AppConstants.primary),
+        ),
+        iconTheme: IconThemeData(color: AppConstants.primary),
+        backgroundColor: AppConstants.bgColor,
+      ),
+      drawer: Drawer(
+        backgroundColor: AppConstants.bgColor,
+        child: ListView(
+          children: [
+            Container(
+              height: 180,
+              padding: const EdgeInsets.only(top: 24),
+              child: Image.asset(AppConstants.emamLogo),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.dashboard_rounded,
+                color: AppConstants.primary,
+              ),
+              title: Text(
+                'D A S H B O A R D',
+                style: GoogleFonts.aBeeZee(color: AppConstants.primary),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
